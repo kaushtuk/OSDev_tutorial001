9a0242f2dd447170fa5e63f088714ba9388bd081
kaushtuk <kaustubhkp10@gmail.com>
2018-03-03 00:27:55 +0530

    Rendering Colorful ASCII Art

--- src/kernel.c
+++ src/kernel.c
@@ -64,7 +64,7 @@ void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_color = vga_entry_color(VGA_COLOR_GREEN, VGA_COLOR_BROWN);
 	terminal_buffer = (uint16_t*) 0xB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -72,6 +72,14 @@ void terminal_initialize(void)
 			terminal_buffer[index] = vga_entry(' ', terminal_color);
 		}
 	}
+	terminal_color = vga_entry_color(VGA_COLOR_MAGENTA, VGA_COLOR_RED);
+    for (size_t y = VGA_HEIGHT/2; y < VGA_HEIGHT; y++) {
+		for (size_t x = VGA_WIDTH/2; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry('$', terminal_color);
+		}
+	}
+	terminal_color = vga_entry_color(VGA_COLOR_GREEN, VGA_COLOR_BROWN);
 }
  
 void terminal_setcolor(uint8_t color) 
